05acde9f0396bac821dc2fec5128d1e853bc424c
xiangdong she <shexd1001@gmail.com>
2016-10-18 00:14:37 +0800

    delete no use code

--- src/main/java/com/person/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.person.service;
-
-import com.person.entity.User;
-
-public interface UserService {
-	void add(User user) throws Exception;
-
-	User findUser(User user) throws Exception;
-}
